84bec8bf8e5285775cc8527e48b7e19b06196dd6
MahaCreative <gunturmadjid.3@gmail.com>
2025-09-21 21:41:17 +0800

    update

--- resources/js/pages/data.jsx
+++ resources/js/pages/data.jsx
@@ -75,6 +75,7 @@ export default function SensorSimulator() {
                 suhu_ruangan: suhuRuangan,
                 kelembapan: kelembapan,
                 suhu_air: suhuAir,
+                status_relay: suhuRuangan > 30 || kelembapan < 80 ? 'on' : 'off',
             };
             console.log('Payload kirim:', payload);
 
@@ -107,19 +108,37 @@ export default function SensorSimulator() {
                 </div>
                 <div>
                     <label className="mb-1 block text-sm font-medium">🎯 Target TDS</label>
-                    <input type="number" name="tds" defaultValue={targetTds ?? ''} className="w-full rounded-md border px-3 py-2" />
+                    <input step="any" type="number" name="tds" defaultValue={targetTds ?? ''} className="w-full rounded-md border px-3 py-2" />
                 </div>
                 <div>
                     <label className="mb-1 block text-sm font-medium">🎯 Target Suhu Ruangan</label>
-                    <input type="number" name="suhu" defaultValue={targetSuhuRuangan ?? ''} className="w-full rounded-md border px-3 py-2" />
+                    <input
+                        step="any"
+                        type="number"
+                        name="suhu"
+                        defaultValue={targetSuhuRuangan ?? ''}
+                        className="w-full rounded-md border px-3 py-2"
+                    />
                 </div>
                 <div>
                     <label className="mb-1 block text-sm font-medium">🎯 Target Kelembapan</label>
-                    <input type="number" name="kelembapan" defaultValue={targetKelembapan ?? ''} className="w-full rounded-md border px-3 py-2" />
+                    <input
+                        step="any"
+                        type="number"
+                        name="kelembapan"
+                        defaultValue={targetKelembapan ?? ''}
+                        className="w-full rounded-md border px-3 py-2"
+                    />
                 </div>
                 <div>
                     <label className="mb-1 block text-sm font-medium">🎯 Target Suhu Air</label>
-                    <input type="number" name="suhu_air" defaultValue={targetSuhuAir ?? ''} className="w-full rounded-md border px-3 py-2" />
+                    <input
+                        step="any"
+                        type="number"
+                        name="suhu_air"
+                        defaultValue={targetSuhuAir ?? ''}
+                        className="w-full rounded-md border px-3 py-2"
+                    />
                 </div>
 
                 <button type="submit" className="w-full rounded-lg bg-blue-600 py-2 font-semibold text-white hover:bg-blue-700">
